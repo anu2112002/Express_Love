1b3c102b83163952455b68538811abe9b19eeba0
Anushri <anushri046@gmail.com>
2024-09-06 20:18:55 +0530

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hey, I wanted to tell you something . </br> Will you be my partner for the rest of my life",
+  descriptionHeader:
+    "Please answer honestly. If you stay silent and leave, that means you AGREE.",
+  buttonYes: "Yes, I Love U",
+  buttonNo: "No, I Don't",
+  titleModar: "Thank you.",
+  descriptionModar:
+    "Thank you for giving me a chance to be by your side, to protect and care for you. </br> I know I'm not perfect, nor very talented, but I have a sincere heart. You are my whole world. </br> Love you <3",
+};
